bbbfbbc989f2f62047526e684c6979e3b6e27597
Christophe Hurpeau <christophe@hurpeau.com>
2014-03-21 22:19:03 +0100

    0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,5 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-
-gulp.task('concat', function() {
-	gulp.src(["./src/es5/html5elements.js", "node_modules/es5-shim/es5-shim.min.js", "node_modules/es5-shim/es5-sham.min.js"])
-		.pipe(concat("es5-compat.js"))
-        .pipe(gulp.dest('dist/'));
-    gulp.src(["node_modules/es6-shim/es6-shim.js"])
-        .pipe(concat("es6-compat.js"))
-        .pipe(gulp.dest('dist/'));
-});
+require('./gulptask.js')(gulp);
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['concat']);
-
-
-gulp.task('build', ['concat']);
+gulp.task('default', ['springbokjs-shim']);
--- gulptask.js
+++ gulptask.js
@@ -1,14 +1,18 @@
 module.exports = function(gulp) {
     var concat = require('gulp-concat');
+    var uglify = require('gulp-uglify');
 
     gulp.task('springbokjs-shim', function() {
         gulp.src(["node_modules/springbokjs-shim/src/es5/html5elements.js",
-                    "node_modules/springbokjs-shim/node_modules/es5-shim/es5-shim.min.js",
-                    "node_modules/springbokjs-shim/node_modules/es5-shim/es5-sham.min.js"])
+                "node_modules/douglascrockford-JSON-js/json2.js", // IE < 8, iOS < 4.0.
+                "node_modules/es5-shim/es5-shim.js",
+                "node_modules/es5-shim/es5-sham.js"])
             .pipe(concat("es5-compat.js"))
+            .pipe(uglify())
             .pipe(gulp.dest('dist/'));
         gulp.src(["node_modules/springbokjs-shim/node_modules/es6-shim/es6-shim.js"])
             .pipe(concat("es6-compat.js"))
+            .pipe(uglify())
             .pipe(gulp.dest('dist/'));
     });
 };
