f39554d8693ca3d9afe9f2e39498d82014deccb4
Christophe Hurpeau <christophe@hurpeau.com>
2014-03-22 22:06:28 +0100

    0.0.7

--- gulptask.js
+++ gulptask.js
@@ -4,9 +4,9 @@ module.exports = function(gulp) {
 
     gulp.task('springbokjs-shim', function() {
         gulp.src(["node_modules/springbokjs-shim/src/es5/html5elements.js",
-                "node_modules/douglascrockford-JSON-js/json2.js", // IE < 8, iOS < 4.0.
-                "node_modules/es5-shim/es5-shim.js",
-                "node_modules/es5-shim/es5-sham.js"])
+                "node_modules/springbokjs-shim/node_modules/douglascrockford-JSON-js/json2.js", // IE < 8, iOS < 4.0.
+                "node_modules/springbokjs-shim/node_modules/es5-shim/es5-shim.js",
+                "node_modules/springbokjs-shim/node_modules/es5-shim/es5-sham.js"])
             .pipe(concat("es5-compat.js"))
             .pipe(uglify())
             .pipe(gulp.dest('dist/'));
